913185c4bb7355f8e779782575d349c1a9ec88bb
Gean Bacinello <geanbacinello197@gmail.com>
2026-06-18 19:15:25 -0300

    exercicio 5

--- src/exercicios/java/Produto_de_dois_números.java
+++ src/exercicios/java/Produto_de_dois_números.java
@@ -1,8 +1,31 @@
 package exercicios.java;
 
+import java.util.Scanner;
+
 /**
- * Gean Corr
+ * Gean Correa Bacinello
+ *
+ * Escreva um programa Java que leva dois números como entrada e exibe o produto de dois números.
+ * Dados do teste:
+ * Entrada primeiro número: 25
+ * Entrada segundo número: 5
+ * Produção esperada :
+ * 25 x 5 = 125
  */
 
 public class Produto_de_dois_números {
+    static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero:");
+        int num1 = in.nextInt();
+
+        System.out.println("Digite o segundo numero: ");
+        int num2 = in.nextInt();
+
+        System.out.println(num1 + "x" + num2 + " = " + num1*num2);
+
+        in.close();
+
+    }
 }

cc25b5d3760eec93bc9c7198d2f561dfc6a0b2de
Gean Bacinello <geanbacinello197@gmail.com>
2026-06-20 11:01:21 -0300

    exercicio 9

---
+++ src/exercicios/java/Avaliação_de_Expressão.java
@@ -0,0 +1,19 @@
+package exercicios.java;
+
+/**
+ * Gean Corrêa Bacinello
+ *
+ * Escreva um programa Java para calcular as expressões especificadas e imprimir a saída.
+ * Dados do teste:
+ * (25,5 * 3.5 - 3,5 * 3.5) / (40,5 - 4.5))
+ * Saída Esperada
+ * 2.13888888888888889
+ */
+public class Avaliação_de_Expressão {
+
+    static void main(String[] args) {
+
+        double rersultado = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.println("Saida: " + rersultado);
+    }
+}

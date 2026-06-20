ffbc28a2df73709f2ce0556263b9e21a2cd77149
Gean Bacinello <geanbacinello197@gmail.com>
2026-06-20 10:55:53 -0300

    exercicio 8

---
+++ src/exercicios/java/Exibição_de_padrão_JAVA.java
@@ -0,0 +1,24 @@
+package exercicios.java;
+
+/**
+ * Gean Corrêa Bacinello
+ *
+ *Escreva um programa Java para exibir o seguinte padrão.
+ * Padrão da amostra:
+ *
+ *    J a v v a
+ *    J a a v v a a
+ * J J aaaaa V V aaaaa
+ *  JJ a a V a a
+ *
+ */
+
+public class Exibição_de_padrão_JAVA {
+    public static void main(String[] args){
+
+            System.out.println("J  a  V    V  a");
+            System.out.println("J  aa  V  V  aa");
+         System.out.println("J  J  aaaa VV  aaaa");
+        System.out.println("JJ   a   a  V  a    a");
+    }
+}

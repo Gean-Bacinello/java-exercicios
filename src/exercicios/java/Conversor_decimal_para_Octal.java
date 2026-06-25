da85a767f9c7f65b7268923d83052a68503f4cc5
Gean Bacinello <geanbacinello197@gmail.com>
2026-06-25 16:34:55 -0300

    exercicio 21

---
+++ src/exercicios/java/Conversor_decimal_para_Octal.java
@@ -0,0 +1,29 @@
+package exercicios.java;
+
+import java.util.Scanner;
+
+public class Conversor_decimal_para_Octal {
+    static void main(String[] args) {
+        int dec_num, rem, quot, i = 1, j;
+        int[] oct_num = new int[100];
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite um numero em decimal: ");
+        dec_num = in.nextInt();
+
+        quot = dec_num;
+
+        while(quot != 0){
+            oct_num[i++] = quot % 8;
+            quot  = quot / 8;
+        }
+
+        System.out.println("Numero em Octal e: ");
+        for (j = i - 1; j > 0; j--) {
+            System.out.println(oct_num[j]);
+        }
+
+        in.close();
+    }
+}
